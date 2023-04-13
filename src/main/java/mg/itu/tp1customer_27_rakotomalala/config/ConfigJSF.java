f40685482179cab646999fcc3f2cfc3a5ea503ef
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-04-13 15:56:54 +0300

    Écriture des pages JSF et des backing beans associés Configuration

---
+++ src/main/java/mg/itu/tp1customer_27_rakotomalala/config/ConfigJSF.java
@@ -0,0 +1,16 @@
+package mg.itu.tp1customer_27_rakotomalala.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author 27_rakotomalala
+ */
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
